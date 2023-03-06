49d3c12a370182c95b53f9bcaf3ceb8abd4bb6c2
FerrariMarc0 <marco1010@live.it>
2023-03-06 22:10:06 +0100

    timing function only

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 'use strict';
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
 
+function disappeared (){
+ document.getElementById('memory').innerHTML = '';
+}
+
+function timerUserNum(){
+    
+    for(let i = 0; i < 5; i++){
+        let userArray= Number(prompt('Inserisci i numeri che hai appena visto'));
+        userNum.push(userArray);
+    }
+}
+
+setTimeout(disappeared, 5000);
+setTimeout(timerUserNum, 7000);
+
+let myNum= [];
+let userNum= [];
+const numInjected= document.getElementById('memory');
+let user;
+for(let i = 0; i < 5; i++){
+    
+    let myArray= getRndInteger(1, 5);
+    myNum.push(myArray);
+}
+console.log(myNum);
+
+
+numInjected.innerText = myNum;
 
